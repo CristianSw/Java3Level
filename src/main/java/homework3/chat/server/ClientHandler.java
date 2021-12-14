6a674b7826e296171d8008e2c24fcfe3fe1019e5
Cristian Dolinta <cristian.sw@protonmail.com>
2021-12-14 21:07:11 +0000

    Added chat history methods for writing and loading history. #5

--- src/main/java/homework3/chat/server/ClientHandler.java
+++ src/main/java/homework3/chat/server/ClientHandler.java
@@ -1,9 +1,9 @@
 package homework3.chat.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class ClientHandler {
@@ -13,6 +13,7 @@ public class ClientHandler {
     private DataOutputStream out;
     private String name;
     private boolean isAuthenticated = false;
+    private String login;
 
     public ClientHandler(Server server, Socket socket) {
         try {
@@ -78,6 +79,7 @@ public class ClientHandler {
                 String username = server
                         .getAuthService()
                         .findUserByLoginAndPassword(credentials[1], credentials[2]);
+                login = credentials[1];
 
 
                 if (username != null) {
@@ -86,6 +88,7 @@ public class ClientHandler {
                         name = username;
                         server.broadcastMessage("Hello, " + username);
                         server.subscribe(this);
+                        loadHistory(credentials[1]);
                         return;
                     } else {
                         sendMessage("This username is busy!");
@@ -110,17 +113,61 @@ public class ClientHandler {
 
     }
 
-    private void history(final String login){
+    public void history(final String login){
+        File file = new File(login+"History");
+        if (!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try (PrintWriter outFile = new PrintWriter(new FileWriter(file,true))){
+            String mayBeMessage = in.readUTF();
+            if (!mayBeMessage.startsWith("-")){
+                outFile.append("Message : ").append(Arrays.toString(mayBeMessage.getBytes(StandardCharsets.UTF_8))).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private void loadHistory(final String login){
+    public void loadHistory(final String login){
+        File file = new File(login+"History");
+        if (!file.exists()){
+            try {
+                file.createNewFile();
+                sendMessage("No local history.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(login+"History"))){
+            StringBuffer stringBuffer = new StringBuffer();
+            int ch;
+            while ((ch = reader.read()) != -1){
+                stringBuffer.append((char) ch);
+                stringBuffer.append(" ");
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+//    public void changeUsername(final String oldUsername, final String newUsername){
+//        try {
+//            //todo
+//        }
+//    }
 
     public void sendMessage(String outboundMessage) {
         try {
             out.writeUTF(outboundMessage);
+            history(login);
         } catch (IOException e) {
             e.printStackTrace();
         }
